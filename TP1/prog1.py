b5bc54d89a959fe9f02dde0e79b511ebe0d9086c
Thierno Sow <thierno.ppc@gmail.com>
2024-09-16 15:32:58 +0200

    ajout argument des entiers

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,17 +1,24 @@
 print ( " Hello , World ! " )
 # prog1.py
 
+
 import fonctions as f
 
 def main():
-    # Saisie des nombres
-    a = int(input("Entrez la base (a) : "))
-    b = int(input("Entrez l'exposant (b) : "))
-    
-    # Appel de la fonction puissance et affichage du résultat
-    res = f.puissance(a, b)
-    print(f"{a} élevé à la puissance {b} est {res}")
+    try:
+        # Saisie des nombres
+        a = int(input("Entrez la base (a) : "))
+        b = int(input("Entrez l'exposant (b) : "))
+        
+        # Appel de la fonction puissance et affichage du résultat
+        res = f.puissance(a, b)
+        print(f"{a} élevé à la puissance {b} est {res}")
+    except ValueError:
+        print("Veuillez entrer des nombres valides.")
+    except TypeError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
+
 
