a2446b9422f94c4413a276c47e1bf054f6c65ced
Thierno Sow <thierno.ppc@gmail.com>
2024-09-16 14:56:50 +0200

    ajout de du code

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,13 @@
 print ( " Hello , World ! " )
+while True:
+    try:
+        # Saisie du nombre
+        nombre = float(input("Entrez un nombre (ou CTRL-C pour quitter) : "))
+        
+        # Calcul et affichage du carré du nombre
+        carre = nombre ** 2
+        print(f"Le carré de {nombre} est {carre}")
+    except ValueError:
+        # Gestion des erreurs si l'entrée n'est pas un nombre
+        print("Ce n'est pas un nombre valide. Veuillez réessayer.")
+
