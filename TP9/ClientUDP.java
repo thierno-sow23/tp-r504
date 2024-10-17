425486de0bb072108abd0f3080af757448006836
M. Sow <aaa@eee.fr>
2024-10-17 11:30:35 +0200

    ajout de files

---
+++ TP9/ClientUDP.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientUDP {
+    public static void main(String[] args) throws IOException {
+        // Récupérer l'adresse IP locale
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println("Adresse = " + addr.getHostName());
+
+        // Chaîne de caractères à envoyer
+        String s = "Hello World!";
+        byte[] data = s.getBytes();
+
+        // Création du paquet UDP avec les données, l'adresse et le port
+        DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
+
+        // Création du socket pour envoyer le paquet
+        DatagramSocket sock = new DatagramSocket();
+        sock.send(packet);
+
+        // Fermeture du socket après envoi
+        sock.close();
+    }
+}
+
---
+++ TP9/ServeurUDP.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class ServeurUDP {
+    public static void main(String[] args) throws IOException {
+        // Création d'un socket pour écouter sur le port 1234
+        DatagramSocket sock = new DatagramSocket(1234);
+
+        // Buffer pour recevoir les données
+        byte[] buffer = new byte[1024];
+
+        System.out.println("Serveur en attente de messages...");
+
+        // Boucle infinie pour traiter les paquets entrants
+        while (true) {
+            // Création d'un paquet pour recevoir les données
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            System.out.println("- Attente de données");
+
+            // Réception du paquet
+            sock.receive(packet);
+
+            // Conversion des données reçues en chaîne de caractères
+            String str = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Message reçu : " + str);
+        }
+    }
+}
+
